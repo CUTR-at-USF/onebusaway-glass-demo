084d0fe4daaddb0c1934ce58d0db1c7100794fc5
Sean J. Barbeau <barbeau@cutr.usf.edu>
2013-12-16 16:06:09 -0500

    Set app name to "OBA Glass" in the MirrorClient.

--- src/main/java/com/google/glassware/MirrorClient.java
+++ src/main/java/com/google/glassware/MirrorClient.java
@@ -46,7 +46,7 @@ public class MirrorClient {
 
   public static Mirror getMirror(Credential credential) {
     return new Mirror.Builder(new NetHttpTransport(), new JacksonFactory(), credential)
-        .setApplicationName("PG Java Starter").build();
+        .setApplicationName("OBA Glass").build();
   }
 
   public static Contact insertContact(Credential credential, Contact contact) throws IOException {
